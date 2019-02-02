15826881f8cd1cd476c96388ed9af43abcb4fca5
Jennifer Gaillard <jengaillard1@gmail.com>
2019-02-02 03:58:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 /* code goes here */
 <div id="hidden-div" style="display:none">301</div>;
-document.querySelector("div#hidden-div").innerText)
+document.querySelector("div#hidden-div").innerText = 301
